ceb1b485dea8da76f7cd3bed8d05143659db556d
a-krichko <a-krichko@list.ru>
2021-06-14 15:45:02 +0300

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return (<header className={'header'}>
+                <img src={"http://pngimg.com/uploads/pokemon_logo/small/pokemon_logo_PNG12.png"} alt={"logo"} />
+            </header>)
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return (<div className={'content'}>
+        <div>
+            <img
+                src="https://lh3.googleusercontent.com/proxy/VFmgIjRyhgGDT-5MIAgKc5XwG8cYWKgEnduZEt2ho5-fqBAtn7h1u7p4a3Bciud6145gE1pYWbAaTkZTgjgsxUGZMb8YKFliL7MEEObEma9CvNek4J5w93Q"
+                alt="main content"/>
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+            <div>
+                new post
+            </div>
+            <div>
+                <div>post0</div>
+                <div>post1</div>
+                <div>post2</div>
+            </div>
+        </div>
+        Main content
+    </div>)
+};
+
+export default Profile;
